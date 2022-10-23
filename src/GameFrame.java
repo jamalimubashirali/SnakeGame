e0f88b580711a5365f426994c700048a96e075d5
Mubashir <21sw071@students.muet.edu.pk>
2022-10-24 00:20:22 +0500

    Game Completed

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,9 +1,11 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class GameFrame extends JFrame {
     GameFrame(){
      this.add(new GamePanel());
      this.setSize(1000,600);
+     ImageIcon image = new ImageIcon("snake.png");
+     this.setIconImage(image.getImage());
      this.setTitle("Snake Game");
      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
      this.setResizable(false);
@@ -11,4 +13,5 @@ public class GameFrame extends JFrame {
      this.setVisible(true);
      this.setLocationRelativeTo(null);
     }
+
 }
